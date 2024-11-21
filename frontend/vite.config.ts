12a481ad5aa79bc79e5e521cf6a26066c34bf36e
Valentina Vuković <valvkvc@gmail.com>
2024-11-21 13:15:33 +0100

    update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,21 +4,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	server: {
-		proxy: {
-			'/api': {
-				target: 'https://api-dev.trace3d.app',
-				changeOrigin: true,
-				secure: true,
-				rewrite: (path) => path.replace(/^\/api/, ''),
-			},
-			'/storage': {
-				target: 'https://storageacceausdev.blob.core.windows.net',
-				changeOrigin: true,
-				secure: true,
-				rewrite: (path) => path.replace(/^\/storage/, ''),
-			},
-		},
-	},
 	plugins: [react(), tsconfigPaths()],
 });
